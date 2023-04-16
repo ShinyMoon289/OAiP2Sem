48a7e5f766aed3f3b9e11bf21a87bc633bc003d5
Илья <76493639+ShinyMoon289@users.noreply.github.com>
2023-04-16 21:46:27 +0300

    Add files via upload

---
+++ Laba12Oaip/Functions.h
@@ -0,0 +1,7 @@
+#pragma once
+#include<stack>
+extern std::stack<int>stek;
+extern void displayElements();
+extern unsigned int getMenu();
+extern void fillStack();
+
---
+++ Laba12Oaip/Laba12.cpp
@@ -0,0 +1,67 @@
+#include"Functions.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	while (true)
+	{
+		switch (getMenu())
+		{
+		case 1:
+		{
+			displayElements();
+			break;
+		}
+		case 2:
+		{
+			fillStack();
+			break;
+		}
+		case 3:
+		{
+			cout << stek.size();
+			break;
+		}
+		case 4:
+		{
+			while (!stek.empty()) stek.pop();
+			break;
+		}
+		case 0:
+		{
+			return 0;
+			break;
+		}
+		}
+	}
+	switch (getMenu())
+	{
+	case 1: 
+	{
+		displayElements();
+		break;
+	}
+	case 2: 
+	{
+		fillStack();
+		break;
+	}
+	case 3:
+	{
+		cout << stek.size();
+		break;
+	}
+	case 4:
+	{
+		while (!stek.empty()) stek.pop();
+		break;
+	}
+	case 0:
+	{
+		return 0;
+		break;
+	}
+	}
+}
+
